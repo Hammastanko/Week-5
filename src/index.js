20ed92dbb031fd442d83e4aea498b45774d25f1a
Hammastanko <45659843+Hammastanko@users.noreply.github.com>
2019-11-02 10:49:01 +0200

    commit test 2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hellotttte test Eero Peltola");
+  res.send("hellotttte test Eero Peltola 2");
 });
 
 app.get("/qwe", (req, res) => {
